737e9f2863742007b17de963e383a13ff0e27c02
Maya B <newmay4@gmail.com>
2024-03-19 21:54:57 +0300

    Added tests

--- functions/getMultiply/getMultiply.test.js
+++ functions/getMultiply/getMultiply.test.js
@@ -1,9 +1,32 @@
-import {getMultiply} from './getMultiply.js';
+import { getMultiply } from './getMultiply.js';
 
-console.log(getMultiply('5', 5) === undefined);
-console.log(getMultiply(5, true) === undefined);
-console.log(getMultiply(-5, 5) === undefined);
-console.log(getMultiply(5, -5) === undefined); 
-console.log(getMultiply(0, 5) === undefined); 
-console.log(getMultiply(5, 0) === undefined); 
-console.log(getMultiply(5, 5) === 25); 
+describe('getMultiply:', () => {
+  it('Unit test 1', () => { expect(getMultiply('5', 5)).toEqual(undefined); });
+  it('Unit test 2', () => { expect(getMultiply(5, true)).toEqual(undefined); });
+  it('Unit test 3', () => { expect(getMultiply(-5, 5)).toEqual(undefined); });
+  it('Unit test 4', () => { expect(getMultiply(0, 5)).toEqual(undefined); });
+  it('Unit test 5', () => { expect(getMultiply(5, 0)).toEqual(undefined); });
+  it('Unit test 6', () => { expect(getMultiply(5, 5)).toEqual(25); });
+});
+
+// console.log(getMultiply('5', 5) === undefined);
+// console.log(getMultiply(5, true) === undefined);
+// console.log(getMultiply(-5, 5) === undefined);
+// console.log(getMultiply(5, -5) === undefined); 
+// console.log(getMultiply(0, 5) === undefined); 
+// console.log(getMultiply(5, 0) === undefined); 
+// console.log(getMultiply(5, 5) === 25); 
+
+// import { getDifference } from './getDifference.js';
+
+// describe('getDifference:', () => {
+//   it('Unit test 1', () => { expect(getDifference('5', 5)).toEqual(undefined); });
+//   it('Unit test 2', () => { expect(getDifference(5, true)).toEqual(undefined); });
+//   it('Unit test 3', () => { expect(getDifference(-5, 5)).toEqual(undefined); });
+//   it('Unit test 4', () => { expect(getDifference(5, -5)).toEqual(undefined); });
+//   it('Unit test 5', () => { expect(getDifference(3, 5)).toEqual(undefined); });
+//   it('Unit test 6', () => { expect(getDifference(5, 3)).toEqual(2); });
+//   it('Unit test 7', () => { expect(getDifference (5, 5)).toEqual(0); });
+//   it('Unit test 8', () => { expect(getDifference(1.5, 5)).toEqual(undefined); });
+//   it('Unit test 9', () => { expect(getDifference (5, 5.5)).toEqual(undefined); });
+// });
